b29de9007b86d3e18d25fb663d6354cb8a6afdf5
George Kadianakis <desnacked@riseup.net>
2012-02-22 16:07:41 -0800

    Remove non-functional partitioning code from morpheus.py.

--- morpheus.py
+++ morpheus.py
@@ -179,76 +179,6 @@ class MorphingMatrixLP:
         os.remove(GLPK_FILENAME)
         os.remove(SOLUTION_FILENAME)
 
-"""This class represents a probability distribution.
-
-'self.distr' contains the probability distribution as a list.
-
-The rest of this class' data is used to implement section
-"3.4 Dealing With Large Sample Spaces" of the Traffic Morphing paper:
-
-'self.partitions' is a list of N_PARTITIONS lists. Each of these lists
-is called a 'partition' because it contains a subset (of size
-N_PARTITION_ELEMENTS) of self.distr elements.
-The partition elements values are adjusted within the partition to
-represent the probability mass function of the partition (see
-example).
-
-'self.repr' is a list of N_PARTITIONS elements. Each of its elements
-is the sum of the probabilities of its respective partition.
-
-EXAMPLE:
-
-A probability distribution of 8 values could be:
-self.distr = [0.2, 0.2, 0.1, 0.2, 0.1, 0.02, 0.08, 0.1]
-
-Splitting the above example into 4 partitions of 2 elements we get:
-self.partitions[0] concerns elements '0.2' and '0.2' which become
-'0.5' and '0.5' within the partition (0.2/0.4 = 0.5).
-self.partitions[1] concerns elements '0.1' and '0.2' which become
-'0.333...' and '0.666...' within the partition (0.1/0.3 = 0.333...).
-...
-
-Finally self.repr contains:
-self.repr[0] = 0.2 + 0.2 = 0.4
-self.repr[1] = 0.1 + 0.2 = 0.3
-...
-"""
-# 20 * 73 = 1460. Sounds like a nice balance.
-N_PARTITIONS = 20
-N_PARTITION_ELEMENTS = 73
-assert(N_PARTITIONS * N_PARTITION_ELEMENTS == MAX_TCP_PAYLOAD_SIZE)
-class Distribution:
-    def __init__(self, distr_list, do_partition):
-        self.distr = distr_list
-
-        if (do_partition):
-            if (len(self.distr) != MAX_TCP_PAYLOAD_SIZE):
-                print "We only support partitioning on distributions " \
-                      "of size %d." % (MAX_TCP_PAYLOAD_SIZE)
-                sys.exit(1)
-
-            self.partitions = [] # x_1 ... x_n
-            self.repr = [] # X'
-            self.__partition()
-
-    """Split the distribution into N_PARTITIONS partitions of
-    N_PARTITION_ELEMENTS elements each. Also fill self.repr."""
-    def __partition(self):
-        start = 0
-        end = N_PARTITION_ELEMENTS
-        for _ in xrange(N_PARTITIONS):
-            tmp = map(decimal.Decimal, self.distr[start:end])
-            tmp_sum = math.fsum(tmp)
-
-            self.partitions.append(map(lambda x: x/tmp_sum, tmp))
-            self.repr.append(tmp_sum)
-
-            start += N_PARTITION_ELEMENTS
-            end += N_PARTITION_ELEMENTS
-
-        assert(start == MAX_TCP_PAYLOAD_SIZE)
-        assert(len(self.partitions[-1]) == N_PARTITION_ELEMENTS)
-
 def string_is_float(string):
     try:
         float(string)
@@ -296,8 +226,6 @@ def usage():
        --source=<source distribution filename>
        --target=<target distribution filename>
        --output=<morphing matrix output filename>
-    and 'arguments' are:
-       --partition : if you want to use partitioning as an optimization.
     """
     sys.exit()
 
@@ -324,46 +252,10 @@ def get_sqmatrix_from_list(list, size):
 
     return ret
 
-"""Startup morpheus with partitioning.
-
-For more information see section
-"3.4 Dealing With Large Sample Spaces" of the Traffic Morphing paper.
-"""
-def startup_with_partitioning(source_distr, target_distr, output):
-    assert(source_distr.repr and target_distr.repr)
-    """Partition morphing matrix.
-    It's the morphing matrix that links a source partition to a target
-    partition."""
-    p_m_m_glpk = MorphingMatrixLP(source_distr.repr,
-                                  target_distr.repr)
-    p_m_m = p_m_m_glpk.harvest()
-
-    assert(source_distr.partitions and target_distr.partitions)
-    """Interpartition matrices.
-    N_PARTITIONS^2 morphing matrices. Each of them advices on how to
-    morph packets from a source partition to a target partition.
-    Since there are N_PARTITIONS^2 of them, we can use them to find
-    how to morph packets from *any* source partition to *any* target
-    partition.
-
-    The interpartition_matrices list contains tuples of the form:
-    (matrix, source, target) where 'source' and 'target' are integers
-    pointing to the partitions that were used to create the morphing
-    'matrix'.
-    """
-    interpartition_matrices = []
-    for i,j in itertools.product(range(N_PARTITIONS),repeat=2):
-        interpartition_glpk = MorphingMatrixLP(source_distr.partitions[i],
-                                               target_distr.partitions[j])
-        interpartition_matrices.append((interpartition_glpk.harvest(), i, j))
-
-    # save them to 'output'
-
-"""Startup vanilla morpheus. Don't do partitioning and other fancy stuff."""
+"""Startup morpheus."""
 def startup(source_distr, target_distr, output):
     """Morphing matrix."""
-    m_m_glpk = MorphingMatrixLP(source_distr.distr,
-                                target_distr.distr)
+    m_m_glpk = MorphingMatrixLP(source_distr, target_distr)
     m_m = m_m_glpk.harvest()
 
     m_size = len(m_m)
@@ -382,7 +274,7 @@ def startup(source_distr, target_distr, output):
 """Entry point"""
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, "s:t:o:p", ["source=", "target=", "output=", "partition"])
+        opts, args = getopt.getopt(argv, "s:t:o:p", ["source=", "target=", "output="])
     except getopt.GetoptError:
         usage()
         sys.exit(1)
@@ -390,7 +282,6 @@ def main(argv):
     source = None
     target = None
     output = None
-    do_partition = False
 
     for opt, arg in opts:
         if opt in ("-s", "--source"):
@@ -399,8 +290,6 @@ def main(argv):
             target = arg
         elif opt in ("-o", "--output"):
             output = arg
-        elif opt in ("-p", "--partition"):
-            do_partition = True
 
     if ((not source) or (not target)):
         print "Please provide a source and a target distribution."
@@ -412,13 +301,10 @@ def main(argv):
         print "Please provide a valid output filename."
         usage()
 
-    source_distr = Distribution(get_distr_from_file(source), do_partition)
-    target_distr = Distribution(get_distr_from_file(target), do_partition)
+    source_distr = get_distr_from_file(source)
+    target_distr = get_distr_from_file(target)
 
-    if (do_partition):
-        startup_with_partitioning(source_distr, target_distr, output)
-    else:
-        startup(source_distr, target_distr, output)
+    startup(source_distr, target_distr, output)
 
 if __name__ == "__main__":
     if (sys.hexversion < 0x02070000):
